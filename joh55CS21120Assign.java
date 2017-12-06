a3221b91baccac55c755a94186c72196159ec733
Joshua Hodges <JOH55@users.noreply.github.com>
2017-12-06 14:52:02 +0000

    Add files via upload

---
+++ joh55CS21120Assign.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+/**
+ * @author Josh
+ * 
+ */
+
+public class joh55CS21120Assign {
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+
+		Compress Compression = new Compress();
+		Compression.printStats();
+	}
+}
